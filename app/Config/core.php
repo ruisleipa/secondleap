934bae2d197b75bc2e87bfc9b307a1528997b511
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2013-04-20 12:28:59 +0300

    Changed cipher seed and salt.

--- app/Config/core.php
+++ app/Config/core.php
@@ -200,12 +200,14 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '74H13AYjorXDg7cXFfOLtnZSFOdtM6wK03BUtq3I');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '56066041107435182218144038771');
+
+
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
