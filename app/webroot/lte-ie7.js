823e26aa7ff72c12288da1bef4dbd8067720e662
Taru Muhonen <taru.muhonen@gmail.com>
2013-04-21 10:36:21 +0300

    Moon and earth html-view added

---
+++ app/webroot/lte-ie7.js
@@ -0,0 +1,35 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-cogs' : '&#xe001;',
+			'icon-rocket' : '&#xe000;',
+			'icon-plus' : '&#xe002;',
+			'icon-hammer' : '&#xe003;',
+			'icon-coin' : '&#xe004;',
+			'icon-key' : '&#xe005;',
+			'icon-lightning' : '&#xe006;',
+			'icon-earth' : '&#xe007;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
